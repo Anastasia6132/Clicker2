1e279e32c2b4a1b27bd79ea8d0ad17a82651c0ee
igonina1999 <igonina1999@yandex-team.ru>
2022-06-04 11:17:33 +0400

    Test commit

--- app/src/main/java/com/example/clicker2/Authorization.java
+++ app/src/main/java/com/example/clicker2/Authorization.java
@@ -16,7 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+//test
 public class Authorization extends AppCompatActivity {
     public static String userLog;
     Button loginBtn, registrationBtn;
